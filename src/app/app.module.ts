aae1a025bba4e00dd6e5d041198d1d8b9701c03c
Emyeeeel <maryamielcatado@gmail.com>
2024-10-15 14:50:49 +0800

    initalize ionic app and create article

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot()],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
